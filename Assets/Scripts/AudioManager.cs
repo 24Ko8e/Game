aa5a602fed10a1321ca21b1879401507c1d6362b
Abhishek <abhishekm942@gmail.com>
2020-01-05 03:06:30 +0530

    05Jan 2020
    added more ui elements-3 sliders. added ui click sound. set up audio mixer. DECIDED NOT TO INCLUDE RADIO BUTTON FOR UDIO TOGGLE.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource UIclicksound;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void click()
+    {
+        UIclicksound.Play();
+    }
+}
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -2,9 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
+    public AudioMixer audiomixer;
+
+    public Slider musicSlider;
+    public Slider gameMusicSlider;
+    public Slider soundSlider;
+
+    public float musicVolume;
+    public float gameMusicVolume;
+    public float soundVolume;
+    private void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1);
+        gameMusicSlider.value = PlayerPrefs.GetFloat("GameMusicVolume", 1);
+        soundSlider.value = PlayerPrefs.GetFloat("SoundVolume", 1);
+
+        /*if (musicSlider.value > 0.0001)
+        {
+            setVolumeMtoggle(true);
+        }
+        else
+            setVolumeMtoggle(false);*/
+
+    }
+
     public void Play()
     {
         SceneManager.LoadScene(1);
@@ -14,5 +40,68 @@ public class UI : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit");
+    }
+
+    public void setVolumeM(float volume)
+    {
+        audiomixer.SetFloat("MusicVolume", Mathf.Log10 (volume) * 20);
+        musicVolume = volume;
+    }
+    /*public void setVolumeMtoggle(bool state)
+    {
+        if (state)
+        {
+            musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1);
+        }
+        if (!state)
+        {
+            PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+            musicSlider.value = 0.0001f;
+        }
+    }*/
+
+    public void setVolumeIGM(float volume)
+    {
+        audiomixer.SetFloat("GameMusicVolume", Mathf.Log10(volume) * 20);
+        gameMusicVolume = volume;
+    }
+    /*public void setVolumeIGMtoggle(bool state)
+    {
+        if (state)
+        {
+            gameMusicSlider.value = PlayerPrefs.GetFloat("GameMusicVolume", 1);
+        }
+        if (!state)
+        {
+            PlayerPrefs.SetFloat("GameMusicVolume", gameMusicVolume);
+            gameMusicSlider.value = 0.0001f;
+        }
+    }*/
+
+    public void setVolumeS(float volume)
+    {
+        audiomixer.SetFloat("SoundVolume", Mathf.Log10(volume) * 20);
+        soundVolume = volume;
+    }
+    /*public void setVolumeStoggle(bool state)
+    {
+        if (state)
+        {
+            soundSlider.value = PlayerPrefs.GetFloat("SoundVolume", 1);
+        }
+        if (!state)
+        {
+            PlayerPrefs.SetFloat("SoundVolume", soundVolume);
+            soundSlider.value = 0.0001f;
+        }
+    }*/
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.SetFloat("GameMusicVolume", gameMusicVolume);
+        PlayerPrefs.SetFloat("SoundVolume", soundVolume);
+
+        PlayerPrefs.Save();
     }
 }
