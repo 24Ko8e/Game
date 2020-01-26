07d4d359f40f3130d04fdc4ed264e2f0f41bbacc
Abhishek <abhishekm942@gmail.com>
2020-01-26 20:12:52 +0530

    26 Jan 2020
    Changed particle waterfall to infinite scrolling.

---
+++ Assets/Assets/waterfall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterfall : MonoBehaviour
+{
+    public float scroll_speed_x = 0.5f;
+    public float scroll_speed_y = 0.5f;
+    MeshRenderer mr;
+
+    Vector2 saved;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mr = GetComponent<MeshRenderer>();
+        saved = mr.sharedMaterial.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Time.time * scroll_speed_x;
+        float y = Time.time * scroll_speed_y;
+
+        Vector2 offset = new Vector2(x,y);
+        mr.sharedMaterial.SetTextureOffset("_MainTex", saved + offset);
+    }
+}
