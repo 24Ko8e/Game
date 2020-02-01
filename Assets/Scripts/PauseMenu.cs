816666611afa5e1e2f2c7866ff17e592c8e1bf94
Abhishek <abhishekm942@gmail.com>
2020-02-01 18:03:26 +0530

    01 Feb 2020
    Changed the project to LWRP succesfully. Lots of major bug fixes with audio slider and options menu scrolling. added materials for water and force field.
    
    Working build.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    public Animation pauseMenuIn;
+    public Animator OptAnim;
 
+    public GameObject options;
+    public Scrollbar OptionsScroll;
 
     public static bool isPaused = false;
     public GameObject audioManager;
@@ -34,7 +37,6 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
         PauseMenuUI.SetActive(true);
-        pauseMenuIn.PlayQueued("PauseIn", QueueMode.PlayNow);
         audioManager.GetComponent<AudioManager>().Paused();
         Time.timeScale = 0f;
         isPaused = true;
@@ -47,5 +49,31 @@ public class PauseMenu : MonoBehaviour
         audioManager.GetComponent<AudioManager>().game = false;
         audioManager.GetComponent<AudioManager>().igmfadeout();
         SceneManager.LoadScene(0);
+    }
+
+    public void OptionsBack()
+    {
+        OptAnim.SetBool("OUT", true);
+        OptAnim.SetBool("IN", false);
+        StartCoroutine(disableOptionsMenu());
+    }
+
+    IEnumerator disableOptionsMenu()
+    {
+        yield return new WaitForSeconds(.25f);
+        options.SetActive(false);
+    }
+
+    public void OnOptionClick()
+    {
+        options.SetActive(true);
+        OptAnim.SetBool("IN", true);
+        OptAnim.SetBool("OUT", false);
+    }
+
+    IEnumerator setscroll()
+    {
+        yield return new WaitForSeconds(0.5f);
+        OptionsScroll.value = 1;
     }
 }
